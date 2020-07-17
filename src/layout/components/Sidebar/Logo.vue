e7467f27b164594647aec30da5c918f08c99a297
cuixiaoyan <56696508@qq.com>
2020-07-17 17:38:09 +0800

    修改默认配置信息

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -26,7 +26,7 @@ export default {
   },
   data() {
     return {
-      title: '若依管理系统',
+      title: 'xxx管理系统',
       logo: logoImg
     }
   }
--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: '若依管理系统',
+  title: 'xxx管理系统',
 
   /**
    * 是否系统布局配置
